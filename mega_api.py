2629eb6d0972a014fea7c645972ab84a5eef7b60
Hossein <hosseinyaqoubib@gmail.com>
2022-06-10 13:49:32 +0430

    add folder files for upload file in mega

--- mega_api.py
+++ mega_api.py
@@ -8,8 +8,12 @@ class MegaUser:
         self.user_info = Users.get_mega_info(user_id)
         self.mega_user = self.mega_user.login(email=self.user_info.mega_username, password=self.user_info.maga_password)
 
-    def upload_file(self, fail_path, upload_to=None):
-        self.mega_user.upload(fail_path, upload_to)
+    def upload_file(self, fail_path):
+        path = self.mega_user.find_path_descriptor('files')
+        if not path:
+            self.mega_user.create_folder('files')
+        path = self.mega_user.find_path_descriptor('files')
+        self.mega_user.upload(fail_path, path[0])
 
     def __str__(self):
         info = self.mega_user.get_user()
